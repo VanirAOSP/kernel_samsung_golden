08fb8c2993917659483494c71a82b22e3744be50
Fred6681 <frederic.cesart@yahoo.fr>
2014-09-27 22:56:17 +0200

    Add Pegasusqplus governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -384,6 +384,9 @@ extern struct cpufreq_governor cpufreq_gov_savagedzen;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVEQ)
 extern struct cpufreq_governor cpufreq_gov_lulzactiveq;
 #define CPUFREQ_DEFAULT_GOVERNOR  	(&cpufreq_gov_lulzactiveq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQPLUS)
+extern struct cpufreq_governor cpufreq_gov_pegasusqplus;
+#define CPUFREQ_DEFAULT_GOVERNOR  	(&cpufreq_gov_pegasusqplus)
 #endif
 
 
